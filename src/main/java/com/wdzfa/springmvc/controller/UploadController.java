f869ac713ab67d5ff44fef41567634f3fadf965f
Wadzifatur Rohmania <wadzifa@Wadzifaturs-MacBook-Air.local>
2025-09-09 22:10:08 +0700

    COnfigure Spring MVC

---
+++ src/main/java/com/wdzfa/springmvc/controller/UploadController.java
@@ -0,0 +1,50 @@
+package com.wdzfa.springmvc.controller;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller // tidak membangun rest api(aplikasi web biasa)
+public class UploadController {
+
+    private static String UPLOADED_PATH = "/Users/wadzifa/Documents/upload/";
+    
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes){
+
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:status";
+        }
+
+        try{
+            byte[] bytes = file.getBytes(); // take file by its byte
+            Path path = Paths.get(UPLOADED_PATH + file.getOriginalFilename()); // path upload location
+            Files.write(path, bytes);
+            redirectAttributes.addFlashAttribute("message", "You successfully upload a file '" + file.getOriginalFilename() + "'");
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+
+        return "redirect:status";
+ 
+    }
+
+    @GetMapping("/status")
+    public String uploadStatus(){
+        return "status";
+    }
+}
